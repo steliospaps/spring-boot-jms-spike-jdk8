57ee727ed909298bfe65612e7b9a1956cdf1aaa3
stelios <stelios05@gmail.com>
2019-04-14 18:04:02 +0100

    directly from spring boot starter website

---
+++ src/main/java/com/github/steliospaps/spring/boot/jms/spike/SpringBootJmsSpikeApplication.java
@@ -0,0 +1,13 @@
+package com.github.steliospaps.spring.boot.jms.spike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJmsSpikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJmsSpikeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/steliospaps/spring/boot/jms/spike/SpringBootJmsSpikeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.steliospaps.spring.boot.jms.spike;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootJmsSpikeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
